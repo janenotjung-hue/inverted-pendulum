d6bcf6ad6ef8138a41338ba97a8ccbd529bfc41f
janenotjung <janejung1221@gmail.com>
2024-02-02 18:33:03 +1300

    added evaluating files

--- code/app.py
+++ code/app.py
@@ -4,34 +4,33 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 
-model = tf.keras.models.load_model('model_versions/ssm/lstm/model.keras')
+model = tf.keras.models.load_model('model_versions_without_normalizing/ssm/dense/cp-0020.ckpt/')
 
 def format_to_array(data, range): 
    df = pd.DataFrame(data, columns=['time', 'theta', 'thetadot', 'x', 'xdot'])
    n = len(df.index)-range
    df = df.drop(columns='time')
-   d = df.drop(df.index[:n])
-   return d.to_numpy()
+   df = df.drop(df.index[:n])
+   return df
 
 def get_prediction(data):
    df = format_to_array(data, 1)
-   input_data = df.reshape(-1, 1, 4)
-   history = model.predict(input_data)
+   df = df.values.reshape(-1, 1, 4)
+   history = model.predict(df)
    res = history[0].tolist()
    return res
 
 def get_conv_prediction(data):
    df = format_to_array(data, 3)
-   input_data = df.reshape(-1, 3, 4)
-   history = model.predict(input_data)
+   df = df.values.reshape(-1, 3, 4)
+   history = model.predict(df)
    res = history[0].tolist() 
    return res
 
 def get_lstm_prediction(data):
    df = format_to_array(data, 10)
-   input_data = df.reshape(-1, 10, 4)
-   history = model.predict(input_data)
-   print(history)
+   df = df.values.reshape(-1, 10, 4)
+   history = model.predict(df)
    res = history[0].tolist() 
    return res
 
@@ -41,9 +40,9 @@ cors = CORS(app)
 @app.route("/receiver", methods=["POST"])
 def postME():
    data = request.get_json() 
-   res = get_lstm_prediction(data)
+   res = get_prediction(data)
    r = jsonify(res)
    return r
 if __name__ == "__main__":
-   app.run(debug=False) 
+   app.run(debug=True) 
    
